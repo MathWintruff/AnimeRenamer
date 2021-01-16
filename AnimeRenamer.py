c72085ef05fe8a67794155f242f88af1e72284e7
MathWintruff <mathwintruffac@gmail.com>
2021-01-16 18:21:28 -0300

    Adding the Class AnimeEpisode

--- AnimeRenamer.py
+++ AnimeRenamer.py
@@ -1,3 +1,7 @@
 from CustomFunctions import *
 
-print(AnimeEpisodeDiscover(list("[AnimesTC] Re.Zero kara Hajimeru Isekai Seikatsu 2nd Season - 07 [1080p]")))
+animeTest = AnimeEpisode("[AnimesTC] Re.Zero kara Hajimeru Isekai Seikatsu 2nd Season - 06 [1080p]")
+
+print(animeTest.rawName)
+print(animeTest.treatedName)
+print(animeTest.episodeNumber)
--- CustomFunctions.py
+++ CustomFunctions.py
@@ -1,26 +1,38 @@
-def AnimeEpisodeDiscover(animeName):
-    animeNameTreated = str()
-    epNumUntreated = str()
-    epNum = str()
-
-    for index in range(animeName.count("[")):
-        bracketsPosition = [animeName.index("["), animeName.index("]")]
-        animeName = animeName[:bracketsPosition[0]] + animeName[bracketsPosition[1]+1:]
-    for index in range(animeName.count("(")):
-        bracketsPosition = [animeName.index("("), animeName.index(")")]
-        animeName = animeName[:bracketsPosition[0]] + animeName[bracketsPosition[1]+1:]
-        
-    animeNameTreated = animeNameTreated.join(animeName)
-
-    if " " in animeNameTreated:
-        animeNameTreated =  animeNameTreated.split()
-    else:
-        animeNameTreated =  animeNameTreated.split("_")
-
-    epNumUntreated = list(animeNameTreated[-1])
+class AnimeEpisode:
     
-    for letter in epNumUntreated:
-        if letter in "0123456789":
-            epNum += letter
+    def __init__(self, Name):
+        self.rawName = Name
+        self.treatedName = str()
+        self.finalName = str()
+        self.seasonNumber = str()
+        self.episodeNumber = str()
+        GetAditionalInfoByRawName(self)
+    
+    def GetAditionalInfoByRawName(self):
+        animeNameTreated = str()
+        epNumUntreated = str()
+        epNum = str()
+        FinalReturn = list()
 
-    return epNum
+        for index in range(self.rawName.count("[")):
+            bracketsPosition = [self.rawName.index("["), self.rawName.index("]")]
+            self.rawName = self.rawName[:bracketsPosition[0]] + self.rawName[bracketsPosition[1]+1:]
+        for index in range(self.rawName.count("(")):
+            bracketsPosition = [self.rawName.index("("), self.rawName.index(")")]
+            self.rawName = self.rawName[:bracketsPosition[0]] + self.rawName[bracketsPosition[1]+1:]
+            
+        animeNameTreated = animeNameTreated.join(self.rawName)
+        self.treatedName = animeNameTreated.strip()
+
+        if " " in animeNameTreated:
+            animeNameTreated =  animeNameTreated.split()
+        else:
+            animeNameTreated =  animeNameTreated.split("_")
+
+        epNumUntreated = list(animeNameTreated[-1])
+        
+        for letter in epNumUntreated:
+            if letter in "0123456789":
+                epNum += letter
+
+        self.episodeNumber = epNum
