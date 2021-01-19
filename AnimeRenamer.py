70591906cee8f11245ea52bd03c3adaff3d2dbf4
MathWintruff <mathwintruffac@gmail.com>
2021-01-19 01:12:36 -0300

    Try Except logging implemented

--- AnimeRenamer.py
+++ AnimeRenamer.py
@@ -1,28 +1,23 @@
 from CustomFunctions import *
 from Logger import *
 import os
+import traceback
 
 allDirectoriesList = os.listdir('./Animes')
 seasonDir = list()
 AnimeEpisodesList = list()
 animeToRename = list()
 
-for animeDir in allDirectoriesList:
-    seasonDir = os.listdir(f'./Animes/{animeDir}')
-    for season in seasonDir:
-        AnimeEpisodesList = os.listdir(f"./Animes/{animeDir}/{season}")
-        for animeEpisode in AnimeEpisodesList:
-            animeToRename.append(AnimeEpisodeClass(animeDir, season, animeEpisode, f"./Animes/{animeDir}/{season}/"))
-
-for episode in animeToRename:
-    os.rename(rf"{episode.path}{episode.rawName}",rf"{episode.path}{episode.finalName}")
-    LogAnInfo(f"The anime:||> {episode.rawName} <|| Was renamed to:||> {episode.finalName} <||")
-
-"""
-import traceback
-
 try:
-    newString = str()
+    for animeDir in allDirectoriesList:
+        seasonDir = os.listdir(f'./Animes/{animeDir}')
+        for season in seasonDir:
+            AnimeEpisodesList = os.listdir(f"./Animes/{animeDir}/{season}")
+            for animeEpisode in AnimeEpisodesList:
+                animeToRename.append(AnimeEpisodeClass(animeDir, season, animeEpisode, f"./Animes/{animeDir}/{season}/"))
+
+    for episode in animeToRename:
+        os.rename(rf"{episode.path}{episode.rawName}",rf"{episode.path}{episode.finalName}")
+        LogAnInfo(f"The anime:||> {episode.rawName} <|| Was renamed to:||> {episode.finalName} <||")
 except:
-    logger.error(traceback.format_exc())
-    """
+    LogAnError(f"\nAn Error has occourred, the traceback of the error is:\n{traceback.format_exc()}\n")
