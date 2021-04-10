cad5549c9cba467eac69e324f80828217981269e
MathWintruff <mathwintruffac@gmail.com>
2021-04-10 16:37:45 -0300

    Logger migrated to OtoPy, progress bar added and some logic changed.

--- AnimeRenamer.py
+++ AnimeRenamer.py
@@ -1,20 +1,43 @@
-from CustomFunctions import *
-from Logger import *
+from AnimeEpisodeClass import *
+from OtoPy.UsefulTools import *
 import os
-import traceback
-from OtoPy.OtomaTools import *
 
-pb = OProgressBar(100)
-pb.PrintProgess(50)
+Logger = OLogger(streamLoggin=False, fileLoggin=True)
+bestLogo =[
+ "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",
+ "@                                                                                                                 @",
+ "@         #########       ################         #########         #######        #######        ########       @",
+ "@      ###############    ################      ###############      ########      ########       #########       @",
+ "@    ###################  ################    ###################    #########    #########       ##########      @",
+ "@   ##################### ################   #####################   ######################      ###########      @",
+ "@  ########       ########     ######       ########       ########  ######################      ############     @",
+ "@ #######           ######     ######      ########          ####### ######################     #############     @",
+ "@ #######           #######    ######      #######           ####### ######################     ######  ######    @",
+ "@ #######           #######    ######      #######           ####### ######  ######  ######    ######   ######    @",
+ "@ ########         #######     ######       #######         ######## ######   ####   ######   ######## ########   @",
+ "@  ########       ########     ######       #########      ########  ######          ######   ##################  @",
+ "@   #####################      ########   ########################   ######          ######  ###################  @",
+ "@    ###################        #################################    ######          ######  #################### @",
+ "@      ###############           ##############################      ######          ###### ######         ###### @",
+ "@         ########                 ############     ########         ######          ###### ######         ###### @",
+ "@                                                                                    ######                       @",
+ "@                                                                                    ######                       @",
+ "@                                                                                    ######                       @",
+ "@                                                                                    ######                       @",
+ "@                                                                                                                 @",
+ "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+]
+
+for line in bestLogo:
+    print(line.replace("@", "%"))
 print()
-exit()
 
 try:
     if "Animes" not in list(os.listdir('.')):
         os.mkdir("Animes")
-        LogAnWarning("Animes folder created")
+        Logger.LogWarning("Animes folder created")
 except:
-    LogAnError(f"An Error has occourred, the traceback of the error is:\n{traceback.format_exc()}")
+    Logger.LogExceptError("An Error has occourred, the traceback of the error is:\n")
 
 
 
@@ -22,21 +45,31 @@ allDirectoriesList = os.listdir('./Animes')
 seasonDir = list()
 AnimeEpisodesList = list()
 animeToRename = list()
+totalAnimesRenamed = 0
 
-LogAnWarning("Starting the Reanaming action")
+Logger.LogInfo("Starting the Reanaming action")
 
 try:
-    for animeDir in allDirectoriesList:
+    GetingAnimesPB = OProgressBar(len(allDirectoriesList), prefix="Getting Animes to rename: ",suffix="Obtained", length=50)
+    for i,animeDir in enumerate(allDirectoriesList):
         seasonDir = os.listdir(f'./Animes/{animeDir}')
+        GetingAnimesPB.PrintProgress(i+1)
         for season in seasonDir:
             AnimeEpisodesList = os.listdir(f"./Animes/{animeDir}/{season}")
             for animeEpisode in AnimeEpisodesList:
                 animeToRename.append(AnimeEpisodeClass(animeDir, season, animeEpisode, f"./Animes/{animeDir}/{season}/"))
+    print("All Animes Obtained!\n")
 
-    for episode in animeToRename:
+    RenamingAnimesPB = OTimedProgressBar(len(animeToRename), Etc=True, prefix="Renaming Animes: ",suffix="")
+    for i,episode in enumerate(animeToRename):
         os.rename(rf"{episode.path}{episode.rawName}",rf"{episode.path}{episode.finalName}")
-        LogAnInfo(f"The anime:||> {episode.rawName} <|| Was renamed to:||> {episode.finalName} <||")
+        Logger.LogInfo(f"The anime:||> {episode.rawName} <|| Was renamed to:||> {episode.finalName} <||")
+        RenamingAnimesPB.PrintProgress(i+1)
+        totalAnimesRenamed = i+1
+    print(f"{totalAnimesRenamed} Anime's Episodes Renamed!")
 except:
-    LogAnError(f"An Error has occourred, the traceback of the error is:\n{traceback.format_exc()}")
+    Logger.LogExceptError("An Error has occourred, the traceback of the error is:\n")
 
-LogAnWarning("Finalizing the Reanaming action")
+Logger.LogInfo(f"{totalAnimesRenamed} Anime's Episodes Renamed! Total time to rename was: {RenamingAnimesPB.GetLasElapsedTime()}")
+
+input("Press enter to close!")
